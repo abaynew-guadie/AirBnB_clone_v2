2da5011d70fe5712fd61f03d382fac22ae0798df
abaynew-guadie <abaynew@yahoo.com>
2023-06-18 08:53:06 +0000

    task 0-10

--- models/__init__.py
+++ models/__init__.py
@@ -12,7 +12,6 @@ if is_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
 storage.reload()
